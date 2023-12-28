1ce9ed109f4851576766057be42cf479c06e5cf4
Kevin Loughead <kvnloughead@gmail.com>
2023-12-27 21:51:43 -0500

    feat: add static assets

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,14 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
+	// Serve files out of ./ui/static directory. The path given should be relative
+	// to the project's root.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Register file server as handler for all URL paths that start with /static/.
+	// Must strip the /static prefix before the request hits the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Print("starting server on :4000")
 
 	err := http.ListenAndServe(":4000", mux)
