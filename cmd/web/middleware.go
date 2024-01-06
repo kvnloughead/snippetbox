1c8500c8b172007aafd783c3398c4988a794ef38
Kevin Loughead <kvnloughead@gmail.com>
2024-01-06 13:50:36 -0500

    feat: request logger middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -18,3 +18,18 @@ func secureHeaders(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ip       = r.RemoteAddr
+			protocol = r.Proto
+			method   = r.Method
+			uri      = r.URL.RequestURI()
+		)
+
+		app.logger.Info("received request", "ip", ip, "protocol", protocol, "method", method, "uri", uri)
+
+		next.ServeHTTP(w, r)
+	})
+}
