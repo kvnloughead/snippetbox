187cdf9955b41ce2f81d71ad2dcbae857ed82685
Kevin Loughead <kvnloughead@gmail.com>
2024-01-06 19:16:10 -0500

    refactor: use httprouter
     - use clean (restful) urls
     - refactor routes
     - use app.notFound every for not found handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,15 +6,13 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/kvnloughead/snippetbox/internal/models"
 )
 
+// Displays home page in response to GET /. If we were using http.ServeMux we
+// would have to check the URL, but with httprouter.Router, "/" is exclusive.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
@@ -30,7 +28,11 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 // View page for the snippet with the given ID.
 // If there's no matching snippet a 404 NotFound response is sent.
 func (app *application) viewSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// Params are stored by httprouter in the request context.
+	params := httprouter.ParamsFromContext(r.Context())
+
+	// Once parsed, params are available by params.ByName().
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -52,17 +54,17 @@ func (app *application) viewSnippet(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, http.StatusOK, "view.tmpl", templateData)
 }
 
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
+
 /*
 Inserts a new record into the database. If successful, redirects the user to
 the corresponding page with a 303 status code.
-*/
-func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
 
+If we were using http.ServeMux, we would have to check the method in this handler.
+*/
+func (app *application) createSnippetPost(w http.ResponseWriter, r *http.Request) {
 	// Temporary dummy data.
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
@@ -76,5 +78,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Redirect to page containing the new snippet.
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,29 +3,42 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 /*
 Returns a servemux that serves files from ./ui/static and contains the following routes:
-  - GET  /
-  - GET  /snippet/view
-  - POST /snippet/create
+  - GET  /										display the home page
+  - GET  /snippet/view/:id    display a specific snippet
+  - GET  /snippet/create      display form to create snippets
+  - POST /snippet/create      create a new snippet
+  - GET  /static/*filepath    serve a static file
 */
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	router := httprouter.New()
+
+	// Use our app.notFound method instead of httprouter's built-in 404 handler.
+	router.NotFound = http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			app.notFound(w)
+		})
 
 	// Serve static files out of ./ui/static directory.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(
+		http.MethodGet,
+		"/static/*filepath",
+		http.StripPrefix("/static", fileServer))
 
 	// Routes
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.viewSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.viewSnippet)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.createSnippet)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.createSnippetPost)
 
-	// Initialize standard set of pre-request middlewares.
+	// Initialize chain of standard pre-request middlewares.
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	return standard.Then(mux)
+	return standard.Then(router)
 }
