9110afcef48d3ec5c0f2be3f231693e077bceb74
Kevin Loughead <kvnloughead@gmail.com>
2024-01-20 13:18:11 -0500

    test: snippetView handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -19,4 +20,65 @@ func TestPing(t *testing.T) {
 	// Verify the status code and body.
 	assert.Equal(t, statusCode, http.StatusOK)
 	assert.Equal(t, body, "OK")
+}
+
+func TestSnippetView(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	tests := []struct {
+		name     string
+		id       string
+		wantCode int
+		wantBody string
+	}{
+		{
+			name:     "Existing",
+			id:       "1",
+			wantCode: http.StatusOK,
+			wantBody: "This is a mock snippet.",
+		},
+		{
+			name:     "Non-existing ID",
+			id:       "999",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Zero ID",
+			id:       "0",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Negative ID",
+			id:       "-1",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Non-integer ID",
+			id:       "1.23",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Non-number ID",
+			id:       "foo",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Empty ID",
+			id:       "",
+			wantCode: http.StatusNotFound,
+		},
+	}
+
+	for _, sub := range tests {
+		t.Run(sub.name, func(t *testing.T) {
+			statusCode, _, body := ts.get(t, "/snippet/view/"+fmt.Sprint(sub.id))
+			assert.Equal(t, statusCode, sub.wantCode)
+			if sub.wantBody != "" {
+				assert.StringContains(t, body, sub.wantBody)
+			}
+		})
+	}
+
 }
--- internal/assert.go
+++ internal/assert.go
@@ -1,6 +1,9 @@
 package assert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	// Test helpers won't be cited in test output.
@@ -8,5 +11,13 @@ func Equal[T comparable](t *testing.T, actual, expected T) {
 
 	if actual != expected {
 		t.Errorf("got %v; want %v", actual, expected)
+	}
+}
+
+func StringContains(t *testing.T, actual, expectedSubstr string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expectedSubstr) {
+		t.Errorf("got %q; expected to contain %q", actual, expectedSubstr)
 	}
 }
