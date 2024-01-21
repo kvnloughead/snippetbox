53588b19ba0339f5d0e428087f3e526e0e33e25a
Kevin Loughead <kvnloughead@gmail.com>
2024-01-21 10:18:40 -0500

    docs: update assertion docstrings

--- internal/assert.go
+++ internal/assert.go
@@ -1,10 +1,12 @@
 package assert
 
 import (
+	"regexp"
 	"strings"
 	"testing"
 )
 
+// A test helper that calls t.Errorf() if actual and expected are not equal.
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	// Test helpers won't be cited in test output.
 	t.Helper()
@@ -14,10 +16,21 @@ func Equal[T comparable](t *testing.T, actual, expected T) {
 	}
 }
 
+// A test helper that calls t.Errorf() if actual doesn't contain expectedSubstr.
 func StringContains(t *testing.T, actual, expectedSubstr string) {
 	t.Helper()
 
 	if !strings.Contains(actual, expectedSubstr) {
 		t.Errorf("got %q; expected to contain %q", actual, expectedSubstr)
+	}
+}
+
+// A test helper that calls t.Errorf() if there are no matches of expectedMatch found in actual.
+func StringContainsMatch(t *testing.T, actual string, expectedMatch *regexp.Regexp) {
+	t.Helper()
+
+	matches := expectedMatch.FindStringSubmatch(actual)
+	if len(matches) < 1 {
+		t.Errorf("got %q; expected to match %q", actual, expectedMatch)
 	}
 }
