8926581e6b023d64fde3e39f4bf74d04c75dd973
Kevin Loughead <kvnloughead@gmail.com>
2024-01-24 21:10:16 -0500

    refactor: rearrange handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,33 +11,12 @@ import (
 	"github.com/kvnloughead/snippetbox/internal/validator"
 )
 
-// A struct for passing data to our templateData struct. Contains all form
-// fields, plus an embedded validator struct. The tags instruct our application
-// wide form decoder on how to map struct fields to markup.
-type snippetCreateForm struct {
-	Title               string     `form:"title"`
-	Content             string     `form:"content"`
-	Expires             int        `form:"expires"`
-	validator.Validator `form:"-"` // "-" tells formDecoder to ignore the field
-}
+//
+// Basic Handlers (ping, home, about)
+//
 
-// A struct for passing data to our templateData struct. Contains all form
-// fields, plus an embedded validator struct. The tags instruct our application
-// wide form decoder on how to map struct fields to markup.
-type userSignupForm struct {
-	Name                string     `form:"name"`
-	Email               string     `form:"email"`
-	Password            string     `form:"password"`
-	validator.Validator `form:"-"` // "-" tells formDecoder to ignore the field
-}
-
-// A struct for passing data to our templateData struct. Contains all form
-// fields, plus an embedded validator struct. The tags instruct our application
-// wide form decoder on how to map struct fields to markup.
-type userLoginForm struct {
-	Email               string     `form:"email"`
-	Password            string     `form:"password"`
-	validator.Validator `form:"-"` // "-" tells formDecoder to ignore the field
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
 }
 
 // Displays home page in response to GET /. If we were using http.ServeMux we
@@ -59,6 +38,18 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) about(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 	app.render(w, r, http.StatusOK, "about.tmpl", data)
+}
+
+//
+// Snippet handlers
+//
+
+// Struct containing form fields for the /snippet/create form.
+type snippetCreateForm struct {
+	Title               string     `form:"title"`
+	Content             string     `form:"content"`
+	Expires             int        `form:"expires"`
+	validator.Validator `form:"-"` // "-" tells formDecoder to ignore the field
 }
 
 // View page for the snippet with the given ID.
@@ -88,6 +79,73 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	data.Snippet = snippet
 
 	app.render(w, r, http.StatusOK, "view.tmpl", data)
+}
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	data.Form = snippetCreateForm{Expires: 365}
+	app.render(w, r, http.StatusOK, "create.tmpl", data)
+}
+
+/*
+Inserts a new record into the database. If successful, redirects the user to
+the corresponding page with a 303 status code.
+
+If one or more fields are invalid, the form is rendered again with a 422 status
+code, displaying the appropriate error messages.
+
+If we were using http.ServeMux, we would have to check the method in this handler.
+*/
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	// Create an instance of our form struct and decode it with decodePostForm.
+	// This automatically parses the values passed as the second argument into the
+	// corresponding struct fields, making appropriate data conversions.
+	var form snippetCreateForm
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// Validate all form fields.
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field can't be blank.")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This can't contain more than 100 characters.")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field can't be blank.")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7, or 365.")
+
+	// If there are any validation errors, render the page again with the errors.
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
+		return
+	}
+
+	// Insert new record or respond with a server error.
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	// Assign text to session data with the key "flash". The data is stored in the
+	// request's context. If there is no current session, a new one will be created.
+	// The flash is added to our template data via the newTemplateData function.
+	app.sessionManager.Put(r.Context(), string(flash), "Snippet successfully created!")
+
+	// Redirect to page containing the new snippet.
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+}
+
+//
+// User handlers
+
+// Struct containing form fields for the /user/signup form.
+type userSignupForm struct {
+	Name                string     `form:"name"`
+	Email               string     `form:"email"`
+	Password            string     `form:"password"`
+	validator.Validator `form:"-"` // "-" tells formDecoder to ignore the field
 }
 
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
@@ -133,6 +191,13 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 	app.sessionManager.Put(r.Context(), string(flash), "Your signup was successful, please log in.")
 
 	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+}
+
+// Struct containing form fields for the /user/login form.
+type userLoginForm struct {
+	Email               string     `form:"email"`
+	Password            string     `form:"password"`
+	validator.Validator `form:"-"` // "-" tells formDecoder to ignore the field
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
@@ -213,61 +278,9 @@ func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	data := app.newTemplateData(r)
-	data.Form = snippetCreateForm{Expires: 365}
-	app.render(w, r, http.StatusOK, "create.tmpl", data)
-}
-
-/*
-Inserts a new record into the database. If successful, redirects the user to
-the corresponding page with a 303 status code.
-
-If one or more fields are invalid, the form is rendered again with a 422 status
-code, displaying the appropriate error messages.
-
-If we were using http.ServeMux, we would have to check the method in this handler.
-*/
-func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// Create an instance of our form struct and decode it with decodePostForm.
-	// This automatically parses the values passed as the second argument into the
-	// corresponding struct fields, making appropriate data conversions.
-	var form snippetCreateForm
-	err := app.decodePostForm(r, &form)
-	if err != nil {
-		app.clientError(w, http.StatusBadRequest)
-		return
-	}
-
-	// Validate all form fields.
-	form.CheckField(validator.NotBlank(form.Title), "title", "This field can't be blank.")
-	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This can't contain more than 100 characters.")
-	form.CheckField(validator.NotBlank(form.Content), "content", "This field can't be blank.")
-	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7, or 365.")
-
-	// If there are any validation errors, render the page again with the errors.
-	if !form.Valid() {
-		data := app.newTemplateData(r)
-		data.Form = form
-		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
-		return
-	}
-
-	// Insert new record or respond with a server error.
-	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
-	if err != nil {
-		app.serverError(w, r, err)
-		return
-	}
-
-	// Assign text to session data with the key "flash". The data is stored in the
-	// request's context. If there is no current session, a new one will be created.
-	// The flash is added to our template data via the newTemplateData function.
-	app.sessionManager.Put(r.Context(), string(flash), "Snippet successfully created!")
-
-	// Redirect to page containing the new snippet.
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
-}
+//
+// Account handlers
+//
 
 // Displays account page in response to GET /account/view.
 func (app *application) accountView(w http.ResponseWriter, r *http.Request) {
@@ -287,8 +300,4 @@ func (app *application) accountView(w http.ResponseWriter, r *http.Request) {
 	data.User = user
 
 	app.render(w, r, http.StatusOK, "account.tmpl", data)
-}
-
-func ping(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("OK"))
 }
